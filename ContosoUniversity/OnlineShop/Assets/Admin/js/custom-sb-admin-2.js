65d38ea10170b0e57dcdfe4e75629c0f6f95fa9a
VU Tuan Trung <trung.lespace@gmail.com>
2021-02-27 12:35:25 +0100

    Adding some login images

---
+++ ContosoUniversity/OnlineShop/Assets/Admin/js/custom-sb-admin-2.js
@@ -0,0 +1,9 @@
+﻿function getRandomLoginImage(from, to) {
+    // Get image by random number
+    const randomNumber = Math.floor(Math.random() * (to - from) + from);
+    const imgSource = `../assets/admin/img/logImages/loginImg_${randomNumber}.jpg`;
+
+    // Set image background
+    var div = document.getElementById("loginImage");
+    div.style.backgroundImage = `url(${imgSource})`;
+};
