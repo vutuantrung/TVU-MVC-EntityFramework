ca6e25246d188d43e4350d1d6f0649d92ba7dc2e
VU Tuan Trung <trung.lespace@gmail.com>
2021-03-05 14:48:28 +0100

    Add Product services

---
+++ Core/eShopSolution/eShopSolution.Application/Catalogies/Products/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.ViewModels.Catalog.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalogies.Products
+{
+    interface IProductService
+    {
+        int Create( ProductCreateRequest request );
+
+        int Update( ProductUpdateRequest request );
+
+        int Delete( int id );
+
+        List<ProductViewModel> GetAll();
+    }
+}
---
+++ Core/eShopSolution/eShopSolution.Application/Catalogies/Products/ProductServices.cs
@@ -0,0 +1,38 @@
+﻿using eShopSolution.Data.EF;
+using eShopSolution.ViewModels.Catalog.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalogies.Products
+{
+    public class ProductServices : IProductService
+    {
+        private readonly EShopDbContext _context;
+
+        public ProductServices( EShopDbContext context )
+        {
+            _context = context;
+        }
+
+        public int Create( ProductCreateRequest request )
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete( int id )
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update( ProductUpdateRequest request )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
